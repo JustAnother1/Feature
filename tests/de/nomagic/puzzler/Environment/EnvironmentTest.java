8ef501249b07e92f34c6009f1ad07d92f3a9f6cc
Lars Pötter <justanotherone@gmx.net>
2019-08-03 23:24:22 +0200

    added first tests for environment.

--- tests/de/nomagic/puzzler/Environment/EnvironmentTest.java
+++ tests/de/nomagic/puzzler/Environment/EnvironmentTest.java
@@ -2,14 +2,48 @@ package de.nomagic.puzzler.Environment;
 
 import static org.junit.Assert.*;
 
-import org.jdom2.Element;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import org.jdom2.Element;
+
 import de.nomagic.puzzler.ContextImpl;
-import de.nomagic.puzzler.ProjectStub;
 import de.nomagic.puzzler.configuration.Configuration;
 
 public class EnvironmentTest
 {
+    Configuration cfg = new Configuration();
+    ContextImpl ctx = new ContextImpl(cfg);
+    Environment dut;
     
+    @Before
+    public void setUp()
+    {
+        dut = new Environment(ctx);
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testEnvironment()
+    {
+        assertNotNull(dut);
+    }
+    
+    @Test
+    public void testloadFromNull()
+    {
+        assertFalse(dut.loadFromElement(null));
+    }
+    
+    @Test
+    public void testloadFromEmptyElement()
+    {
+        Element e = new Element("env");
+        assertFalse(dut.loadFromElement(e));
+    }
 }
