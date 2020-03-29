b3f8340337c0860db8016128a8510cfd346b1c33
Lars Pötter <justanotherone@gmx.net>
2020-03-29 23:35:26 +0200

    show settings in settings Dialog.

--- gui/configuration.py
+++ gui/configuration.py
@@ -13,34 +13,43 @@ class Configuration(object):
         '''
         self.filename = source
         if 'native' == cfg_type:
-            self.data = QSettings(QSettings.NativeFormat, QSettings.UserScope, 'nomagic', 'LDbg')
+            self.data = QSettings(QSettings.NativeFormat, QSettings.UserScope, 'nomagic', 'Feature')
         elif 'ini'  == cfg_type:
             self.data = QSettings(source, QSettings.IniFormat)
         else:
             self.data = None
-            
-        
+
+
     def writeToFile(self):
         if None != self.data:
             self.data.sync()
-            
+
     def getInt(self, settingName, default):
         if None != self.data:
             if None != settingName:
-                return self.data.value(settingName, defaultValue=default, type=int)
+                res = self.data.value(settingName, defaultValue=default, type=int)
+                if res == default:
+                    self.data.setValue(settingName, res)
+                return res
         return default
-    
+
     def setInt(self, which, value):
         if None != self.data:
             self.data.setValue(which, value)
-    
+
     def setString(self, which, value):
         if None != self.data:
             self.data.setValue(which, value)
-            
+
     def getString(self, which, defaultValue = ''):
         if None == self.data:
             return defaultValue
-        
-        return str(self.data.value(which, defaultValue))
-            
+        res = str(self.data.value(which, defaultValue))
+        if res == defaultValue:
+            self.data.setValue(which, defaultValue)
+        return res
+
+    def getAllSettings(self):
+        if None == self.data:
+            return []
+        return self.data.allKeys()
--- gui/settings.py
+++ gui/settings.py
@@ -2,10 +2,21 @@ from PyQt5.QtWidgets import QDialog
 from PyQt5.QtCore import qDebug
 from PyQt5.QtWidgets import QDialogButtonBox
 from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QScrollArea
+from PyQt5.QtWidgets import QGroupBox
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QLineEdit
+
 
 class SettingsDialog(QDialog):
 
     settings = None
+    formLayout = None
+    buttonBox = None
+    labelLis = []
+    editList = []
 
     def __init__(self, *args, **kwargs):
         super(SettingsDialog, self).__init__(*args, **kwargs)
@@ -16,22 +27,44 @@ class SettingsDialog(QDialog):
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
+        self.formLayout = QFormLayout()
+        groupBox = QGroupBox("Personal Settings")
+        groupBox.setLayout(self.formLayout)
+        scroll = QScrollArea()
+        scroll.setWidget(groupBox)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(400)
+        layout = QVBoxLayout(self)
+        layout.addWidget(scroll)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
 
     def addSettings(self, newSettings):
+        if None == newSettings:
+            return
         self.settings = newSettings
+        allSettings = newSettings.getAllSettings()
+        for i in range(len(allSettings)):
+            self.labelLis.append(QLabel(allSettings[i]))
+            #self.editList.append(QPushButton("Click Me"))
+            qle = QLineEdit(self)
+#            qle.textChanged[str].connect(self.onChanged)
+            qle.setText(self.settings.getString(allSettings[i]))
+            self.editList.append(qle)
+            self.formLayout.addRow(self.labelLis[i], self.editList[i])
 
     def getEditedSettings(self):
         return self.settings
 
-
+#    def onChanged(self, text):
+#        self.lbl.setText(text)
+#        self.lbl.adjustSize()
 
 # called from main window
 def showSettingsDialog(wnd, cfg):
     qDebug("schowing Settings")
     dlg = SettingsDialog(wnd)
+    dlg.addSettings(cfg)
 
     if dlg.exec_():
         qDebug("Success!")
