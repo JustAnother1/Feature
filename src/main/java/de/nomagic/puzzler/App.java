e1b5a4169eab0dda0912a5f19bf5486ebd580122
Lars Pötter <lars@ing-poetter.de>
2021-05-07 23:14:31 +0200

    moved into Maven folder structure.

---
+++ src/main/java/de/nomagic/puzzler/App.java
@@ -0,0 +1,13 @@
+package de.nomagic.puzzler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
